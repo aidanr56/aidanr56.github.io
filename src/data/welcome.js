77a0d9c596c636492faf6295759fa35fe600a13f
ShadowAssasin00 <aidan.james.roche@gmail.com>
2024-03-15 23:59:25 +0000

    updated welcome interaction

--- src/data/welcome.js
+++ src/data/welcome.js
@@ -1,45 +1,42 @@
 const welcome = [
-    //xp requirements and rewards
-    {   
+    {
         //requirements
         ethics_requirement: 0,
         explainability_requirement: 0,
         data_requirement: 0,
         //rewards
-        ethics_reward: 10,
+        ethics_reward: 20,
         explainability_reward: 0,
-        data_reward: 10,
+        data_reward: 20,
         //other
         interaction_complete: false
     },
-    //node 1
     {
         node: 1,
-        text: "Hi, my name is Sarah! Welcome player to the game. Give them a very brief initial introduction to what AI is. AI refers to technology that enables computers and machines to simulate human-like intelligence, and often encompasses the field of machine and deep learning. This is accomplished by mathematically evaluating input data, and producing a corresponding prediction based on this For example, statistical algorithms, such as linear regression, may be used to find links between trends. Let's try an example, here is a table that shows the price of an apple and the weather for seven consecutive days:",
+        text: "Hello Traveller! Welcome to AI-Landia, my name is Sarah. We're so glad you were able to make it here, I hope your journey went well. All of us here on AI-Landia are passionate about the world of AI, and can't wait to share our knowledge with you. Currently, we have experts on Ethics of AI, Explainable AI, and Data Literacy. As you explore this island feel free to approach any of the locals, they'd love to have a chat. But first, let's start with a little introduction on AI to make sure your ready to go. Sound good?",
         responses: [
             {
                 text: 'Continue',
-                correct: null,
                 nextNode: 2
             }
         ]
     },
     {
         node: 2,
-        text: "DAY --- COST --- WEATHER \n 1 ---    5.00 --- Light Rain \n 2 ---    8.00 --- Partially Cloudy \n 3 ---    3.00 --- Stormy \n 4 ---    12.00 --- Sunny",
-        responses : [
+        text: "AI has been a pretty big buzz word, it feels like everyone is talking about it. In case you weren't sure, AI stands for Artificial Intelligence. Generally speaking, AI refers to the fields of Machine Learning and/or Deep Learning, but of which are sub-fields of computer science. Put simply, these fields are concerned with mathematically evaluating large sets of data, to consequently make predictions or classifications. An example of this could be predicting how much a house will cost. For example, we might give an AI a set of data that has information on thousands of houses. For each house, we could include its price, location, size, number of bedrooms, and number of bathrooms. Now let's say in our training data we saw that a house with 2 bedrooms cost 100,000 and a house with 3 bedrooms cost 150,000. Do you think a house with 4 bedrooms will cost more or less than 150,000?",
+        responses: [
             {
-                text: "There is no connection between the weather and the cost of apples.",
-                correct: false,
-                nextNode: 4
-            },
-            {
-                text: "The nicer the weather, the more expensive the apples.",
+                text: "The house with 4 bedrooms will cost more",
                 correct: true,
                 nextNode: 3
             },
             {
-                text: "The worse the weather, the more expensive the apples.",
+                text: "The house with 4 bedrooms will cost the same",
+                correct: false,
+                nextNode: 4
+            },
+            {
+                text: "The house with 4 bedrooms will cost less",
                 correct: false,
                 nextNode: 4
             }
@@ -47,73 +44,37 @@ const welcome = [
     },
     {
         node: 3,
-        text: "That is correct, as you saw, the apples are more expensive on days with nicer weather. Now let's try an exercise, this time we'll show you a table, but without the price. Can you tell me what day the apples will probably be the cheapest?",
+        text: "That's exactly right, and that's similar to how an AI would view this problem. The system would be able to see that as the number of bedrooms increases, so does the cost. More complex systems might take into account a larger number of 'features'. Features refers to the components of the dataset, in this case, the price, location, size, number of bedrooms/bathrooms in the house.",
         responses: [
             {
-                text: "Continue",
-                correct: null,
+                text: 'Continue',
                 nextNode: 5
             }
         ]
     },
     {
         node: 4,
-        text: "Unfortunately, that wasn't quite right. In the previously shown table, the cost of Apples was actually higher on days with nicer weather (i.e. Sunny days). It's important to remember that we are not trying to differentiate between correlation and causation here. While it is true that the nice weather may not be the reason for the change in price, there is still a correlation. Now, based on this, let's try another example. Except this time I want you to tell me which days you think the Apples will be cheapest. (Keep in mind we are basing this off the data in the previous example).",
-        responses: [
-            {
-                text: "Continue",
-                correct: null,
-                nextNode: 5
-            }
-        ]
+        text: "Unfortunately, I'd have to disagree. Based on the information we were given, it appears that as the number of bedrooms increases, so does the cost of the house. Based on this we can predict that a house with 4 bedrooms will cost more than a house with 3, so long as we have no other information to go off. This is similar to how an AI would view this problem. The system would be able to see that as the number of bedrooms increases, so does the cost. More complex systems might take into account a larger number of 'features'. Features refers to the components of the dataset, in this case, the price, location, size, number of bedrooms/bathrooms in the house."
     },
     {
         node: 5,
-        text: "DAY --- WEATHER \n 1 ---     Sunny \n 2 ---      Partially Cloudy \n 3 ---       Stormy \n 4 --      Light Rain",
+        text: "I hope that example was able to get you thinking about how we can make predictions based on past data. It's a very primitive example of how some AI systems work. However, it is important to note that AI is a broad term. Every AI system works differently, and is designed to accomplish a different goal. While the definition of AI is not exactly clear the consensus seems to be that it refers to technology that has the capability to simulate human-like intelligence. However, most contemporary AI systems are quite niche, so called narrow AI. This means that they are capable of dealing with a specific set of tasks, and are unable to complete tasks beyond what they were explicitly trained on.",
         responses: [
             {
-                text: "Day 1",
-                correct: false,
-                nextNode: 7
-            },
-            {
-                text: "Day 2",
-                correct: false,
-                nextNode: 7
-            },
-            {
-                text: "Day 3",
-                correct: true,
+                text: 'Continue',
                 nextNode: 6
-            },
-            {
-                text: "Day 4",
-                correct: false,
-                nextNode: 7
             }
         ]
     },
     {
         node: 6,
-        text: "That was correct, well done. This is a very primitive example of how an AI might work. Modern AI’s such as ChatGPT use vast “neural networks” that take into account millions of different parameters to make predictions. Don’t worry if this seems confusing, for now, it is time for you to begin your adventure. AI-landia is filled with helpful people, try talking to them, and I have no doubt you’ll learn something new!",
+        text: "Now, that's all I have for you. I just wanted to give you a little introduction. For now, feel free to explore the island some more. Try and talk to as many people as you can, they all have interesting stuff to say, and I'm sure you'd learn a lot. Good luck out there!",
         responses: [
             {
-                text: "Leave",
-                correct: null,
+                text: "Leave"
             }
         ]
-    },
-    {
-        node: 7,
-        text: "That wasn't quite right, let's try that one again. Remember what we saw in this first table. As the weather was increasing, so was the price of the apples.",
-        responses: [
-            {
-                text: "Try Again",
-                correct: null,
-                nextNode: 5
-            }
-        ]
-    },
+    }
 ]
 
 export { welcome }
